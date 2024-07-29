c4410919549d20374a4f713bfcfc387dedce3324
matshidisolp <mpitswane@gmail.com>
2024-07-29 07:35:59 +0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Start Flask"""
-
+"""Start a simple web Flask application."""
 
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display Hello HBNB!"""
+    """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display HBN"""
+    """Display 'HBNB'"""
     return "HBNB"
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """Display /c/<text>"""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
-
+    """Display 'C <text>' with underscores replaced by spaces"""
+    return "C {}".format(text.replace("_", " "))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
